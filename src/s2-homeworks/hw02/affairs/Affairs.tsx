ba2f39ea7eb4c66f5afc5ea44fd50ba326801c45
nedonebo21 <cursed.void10007@gmail.com>
2025-05-28 19:10:21 +0400

    homework sprint-2 week-2(done)

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,24 +4,27 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[] // need to fix any
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
     filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
     const setAll = () => {
-        // need to fix
+        props.setFilter('all')
     }
     const setHigh = () => {
         // need to fix
+        props.setFilter('high')
     }
     const setMiddle = () => {
         // need to fix
+        props.setFilter('middle')
     }
     const setLow = () => {
         // need to fix
+        props.setFilter('low')
     }
 
     const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
@@ -43,30 +46,26 @@ function Affairs(props: AffairsPropsType) {
                 <button
                     id={'hw2-button-all'}
                     onClick={setAll}
-                    className={cnAll}
-                >
+                    className={cnAll}>
                     All
+                </button>
+                <button
+                    id={'hw2-button-low'}
+                    onClick={setLow}
+                    className={cnLow}>
+                    Low
                 </button>
                 <button
                     id={'hw2-button-high'}
                     onClick={setHigh}
-                    className={cnHigh}
-                >
+                    className={cnHigh}>
                     High
                 </button>
                 <button
                     id={'hw2-button-middle'}
                     onClick={setMiddle}
-                    className={cnMiddle}
-                >
+                    className={cnMiddle}>
                     Middle
-                </button>
-                <button
-                    id={'hw2-button-low'}
-                    onClick={setLow}
-                    className={cnLow}
-                >
-                    Low
                 </button>
             </div>
             <div className={s.affairs}>{mappedAffairs}</div>
