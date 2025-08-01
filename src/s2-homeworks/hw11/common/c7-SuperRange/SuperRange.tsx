c04c46b5a1fac95dc19c202d41f6c0d84e81228e
nedonebo21 <cursed.void10007@gmail.com>
2025-08-01 17:12:54 +0400

    hw sprint-4 week-3

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -2,14 +2,46 @@ import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+  return (
+      <Slider
+          sx={{ // стили для слайдера // пишет студент
+            width: '147px',
+            color: '#00CC22',
+            '& .MuiSlider-thumb': {
+              width: '18px',
+              height: '18px',
+              backgroundColor: '#fff',
+              border: '1px solid #00CC22',
+              boxSizing: 'border-box',
+
+              '&::before': {
+                content: '""',
+                position: 'absolute',
+                boxShadow: 'none',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: '6px',
+                height: '6px',
+                borderRadius: '50%',
+                backgroundColor: '#00cc22',
+                zIndex: 1,
+              }
+            },
+            '& .MuiSlider-thumb: nth-of-type(1)': {
+              position: 'relative'
+            },
+            '& .MuiSlider-rail': {
+              backgroundColor: '#8b8b8b',
+              opacity: 1
+            },
+            '& .MuiSlider-track': {
+              backgroundColor: '#00cc22'
+            }
+          }}
+          {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+      />
+  )
 }
 
 export default SuperRange
