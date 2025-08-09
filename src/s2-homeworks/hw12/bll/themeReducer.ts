13279dfe13cd5665ff782456b44d1f0c36972bee
nedonebo21 <cursed.void10007@gmail.com>
2025-08-09 15:12:59 +0400

    hw sprint-4 week-4

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,18 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeActionType): typeof initState=> { // fix any
     switch (action.type) {
         // дописать
-
+        case "SET_THEME_ID":
+            return {...state, themeId: action.id}
         default:
             return state
     }
 }
 
 export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeActionType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
